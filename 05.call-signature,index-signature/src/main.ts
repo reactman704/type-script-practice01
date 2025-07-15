d5bafdb11290dbd45bc7406691598bc4a79d8a89
Greatteacherjubro <wisdomjh100@gmail.com>
2025-07-15 23:37:55 +0900

    signature add

---
+++ 05.call-signature,index-signature/src/main.ts
@@ -0,0 +1,41 @@
+
+// interface getLikeNumber {
+//   (like: number): number;
+// }
+
+// interface Post {
+//   id: number;
+//   title: string;
+//   getLikeNumber: getLikeNumber
+// }
+
+// const post1:Post = {
+//   id: 1,
+//   title: 'post1',
+//   getLikeNumber(like: number){
+//     return like
+//   }
+// }
+
+// post1.getLikeNumber(1);
+
+
+interface Post{
+  [key: string]: unknown;
+  id: number;
+  title: string;
+}
+
+const post1: Post = {
+  id: 1,
+  title: 'post1'
+}
+
+post1['description'] = 'description 1';
+post1['pages'] = 300;
+
+interface Names{
+  [item: number]: string;
+}
+
+const userNames: Names = ['John', 'Kim', 'Joe']
