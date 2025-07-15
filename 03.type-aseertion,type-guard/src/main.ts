186b0bea58bc83b9ab9bedd654e26f716ff90785
Greatteacherjubro <wisdomjh100@gmail.com>
2025-07-15 22:57:00 +0900

    03.04 type-assertion, typeguard, type, interface Add

---
+++ 03.type-aseertion,type-guard/src/main.ts
@@ -0,0 +1,16 @@
+
+// const bodyElement = document.querySelector('body');
+// if(bodyElement){
+//   bodyElement.innerText = "Hello";
+// }
+
+
+
+// 잘못 쓴 예시
+// function func(arg: string | null){
+//   if(arg){
+//     return (arg as string).toLowerCase();
+//   }
+// }
+// func('hello');
+// func(null);
---
+++ 04.type,inertface/src/main.ts
@@ -0,0 +1,55 @@
+
+// interface Animal {
+//   name: string;
+// }
+
+
+// interface Bear extends Animal {
+//   honey: boolean;
+// }
+
+// const bear1: Bear = {
+//   name: 'honey Bear',
+//   honey: true
+// }
+
+// type
+// type Animal = {
+//   name: string
+// }
+
+// type Bear = Animal &{
+//   honey: boolean;
+// }
+
+
+// const bear1: Bear = {
+//   name: "honey bear",
+//   honey: true
+// }
+
+
+// interface Animal {
+//   name: string
+// }
+
+// interface Animal{
+//   honey: boolean
+// }
+
+// const bear1: Animal = {
+//   name: 'honey bear',
+//   honey: true
+// }
+
+
+// type은 이렇게 하면 병합이 안됨
+// type Animal = {
+//   name: string
+// }
+
+// type Animal = {
+//   honey: boolean
+// }
+
+
